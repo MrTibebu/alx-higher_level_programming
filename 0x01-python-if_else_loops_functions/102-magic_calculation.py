0e2599257206b68aa190b8d73c89950dfffb1000
MrTibebu <abenezerhailutibebuasfaw@gmail.com>
2023-03-09 13:01:21 -0800

    0x02. Python - import & modules

---
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+#Disassembler for Python bytecode Library used
+def magic_calculation(a, b, c):
+    if a < b:
+        return (c)
+    if c > b:
+        return (a + b)
+    return (a*b - c)
